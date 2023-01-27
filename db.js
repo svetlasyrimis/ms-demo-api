97c6da81429c340777c947d8244fa798ec7b5f2b
svetlasyrimis <svetla.syrimis@gmail.com>
2023-01-27 12:19:04 -0500

    init

---
+++ db.js
@@ -0,0 +1,11 @@
+const outdoorUnits=require('../assets/outdoor-units.json');
+const marinaUnits=require('../assets/marina-units.json');
+const buildingUnits=require('../assets/building-units.json');
+const sites=require('./assets/sites.json');
+const contacts = require('./assets/contacts.json');
+const reservations = require('./assets/reservations.json');
+const leases = require('./assets/leases.json');
+
+module.exports = () => ({
+    outdoorUnits, marinaUnits, buildingUnits, sites, contacts, reservations, leases
+  });
