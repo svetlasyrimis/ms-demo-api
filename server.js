7ed75c0877258adccaf21b55cb64b2d9c7dc5ed5
svetlasyrimis <svetla.syrimis@gmail.com>
2023-01-27 12:26:03 -0500

    init 2

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.js');
+
+const data = require('./db.js')();
+
+fs.writeFile ("db.json", JSON.stringify(db), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    }
+);
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
